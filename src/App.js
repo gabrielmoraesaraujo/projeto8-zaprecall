33c532671c7db318310e5f1a8e44803df4854a18
Gabriel Moraes <gmoraes.mat@gmail.com>
2022-07-23 22:04:05 -0300

    Primeiro commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import Inicio from "./components/Inicio"
+
+export default function App(){
+
+    return(
+        <>
+            < Inicio />
+        </>
+    );
+}
---
+++ src/components/Inicio.js
@@ -0,0 +1,8 @@
+export default function Inicio(){
+    
+
+    return(
+
+        <h1>Inicio</h1>
+    );
+}
