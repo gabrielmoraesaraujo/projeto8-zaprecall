aca87e6386688cbd5a4d6ab4bc3ed4ae7e8938cf
Gabriel Moraes <gmoraes.mat@gmail.com>
2022-07-25 16:45:56 -0300

    Concluidos

---
+++ src/components/Concluido.js
@@ -0,0 +1,27 @@
+import React from "react";
+import party from "../assets/party.png"
+import sad from "../assets/sad.png"
+
+export default function Concluido(props){
+
+
+    for(let i=0;i<props.zaps.length;i++){
+        console.log(props.zaps[i]);
+        if(props.zaps[i] === "naoLembrei"){
+            return(
+                <div className="concluido">
+                    <span><img src={sad} alt=""/><strong>Puts...</strong></span>
+                    Ainda faltam alguns... Mas não desanime!
+                </div>
+                ); 
+               
+        }
+    }
+            return(
+                <div className="concluido">
+                    <span><img src={party} alt=""/><strong>Parabéns!</strong></span>
+                    Você não esqueceu de nenhum flashcard!
+                </div>
+                ); 
+
+}
